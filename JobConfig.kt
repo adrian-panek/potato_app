fac43825a36b88e6b48117ab522c85b47d89b775
adrian-panek <adrian.panek999@gmail.com>
2023-08-15 16:41:54 +0200

    Added latest working version of job

--- JobConfig.kt
+++ JobConfig.kt
@@ -12,45 +12,50 @@ object TestProject_Build : BuildType({
     vcs {
         root(TestProject_FlaskApp)
     }
-steps {
-    script {
-        name = "List all downloaded files"
-        scriptContent = "ls -a"
-        dockerImage = "python"
-        dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-    }
-    script {
-        name = "Update dependency manager"
-        scriptContent = "pip install --upgrade pip"
-        dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-    }
-    script {
-        name = "Create virual environment"
-        scriptContent = "python3 -m venv venv && . venv/bin/activate"
-        dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-    }
-    script {
-        name = "Install all required dependencies"
-        scriptContent = "pip install -r requirements.txt"
-        dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-    }
-    script {
-        name = "Run static code analysis"
-        scriptContent = """find -name "*.py" -not -path "./venv/*" | xargs pylint -E --load-plugins=pylint_flask | tee pylint.log"""
-        dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-    }
-    script {
-        name = "Run tests"
-        scriptContent = "python3 -m pytest"
-        dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-    }
-    dockerCommand {
-        name = "Build image"
-        executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
-        commandType = build {
-            source = file {
-                path = "Dockerfile"
+
+    steps {
+        script {
+            name = "List all downloaded files"
+            scriptContent = "ls -a"
+            dockerImage = "python"
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+        }
+        script {
+            name = "Update dependency manager"
+            scriptContent = "pip install --upgrade pip"
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+        }
+        script {
+            name = "Cleanup previous pip installation"
+            scriptContent = "cat requirements.txt | sudo xargs pip uninstall -y"
+        }
+        script {
+            name = "Cleanup previous Docker image build"
+            scriptContent = "docker rmi adrianpanek/python-app:latest"
+        }
+        script {
+            name = "Install all required dependencies"
+            scriptContent = "pip install -r requirements.txt"
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+        }
+        script {
+            name = "Run static code analysis"
+            scriptContent = """find -name "*.py" -not -path "./venv/*" | xargs pylint -E --load-plugins=pylint_flask | tee pylint.log"""
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+        }
+        script {
+            name = "Run tests"
+            scriptContent = "python3 -m pytest"
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+        }
+        dockerCommand {
+            name = "Build image"
+            executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
+            commandType = build {
+                source = file {
+                    path = "Dockerfile"
+                }
             }
         }
     }
-}
+})
